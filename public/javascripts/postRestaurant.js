27feaea6e646eee8a7c9c02d1267725f9fd77ce1
Sam Dicken <sjdick04@louisville.edu>
2014-05-10 17:21:59 -0400

    Use spaces instead of tabs in newly created JS file.

--- public/javascripts/postRestaurant.js
+++ public/javascripts/postRestaurant.js
@@ -1,15 +1,15 @@
 $("form").submit(function()
 {
-	var req = "";
-	var first = true;
-	$("input").each(function(key, value)
-	{
-		if(!first)
-			req += ",";
-		var keyVal = key + ":" + value;
-		req += keyVal;
-		if(first)
-			first = false;
-	});
-	alert(req);
+  var req = "";
+  var first = true;
+  $("input").each(function(key, value)
+  {
+  if(!first)
+    req += ",";
+  var keyVal = key + ":" + value;
+  req += keyVal;
+  if(first)
+    first = false;
+  });
+  alert(req);
 });
