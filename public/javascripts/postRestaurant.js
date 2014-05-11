525a37da05e1d84d621b6338812fb9305c697bf8
Sam Dicken <sjdick04@louisville.edu>
2014-05-10 23:19:25 -0400

    Try fixing external script up and using script externally.

--- public/javascripts/postRestaurant.js
+++ public/javascripts/postRestaurant.js
@@ -1,13 +1,14 @@
-$("form").submit(function(event)
+$("#restaurantAddForm").submit(function(event)
 {
+  console.log("I AM BEING CALLED. YOU ARE NOT CRAZY.");
   // stop form from submitting normally
   event.preventDefault();
   
   var $form = $(this), 
-    restaurantName = $form.attr("input[name='name']").val(),
-	restaurantAddress = $form.attr("input[name='address]").val(),
-	restaurantEmail = $form.attr("input[name='email']").val(),
-	restaurantPhone = $form.attr("input[name='phone']").val(),
+    restaurantName = $form.find("input[name='name']").val(),
+    restaurantAddress = $form.find("input[name='address']").val(),
+    restaurantEmail = $form.find("input[name='email']").val(),
+    restaurantPhone = $form.find("input[name='phone']").val(),
     url = $form.attr("action");
   
   $.post(url, { name: restaurantName, addresses: [ name: "default", line1: restaurantAddress, line2: null, city: "Louisville", state: "KY", zip: "40203" ], emails: [ name: "default", address: restaurantEmail ], phones: [ name: "default", number: restaurantPhone ] });
